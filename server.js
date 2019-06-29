e4017db84512fc3731e4fcf51bd3c5d89b222c98
Salvi <salviN@emea.travelex.net>
2019-06-29 13:23:31 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+const port = process.env.PORT || 3030;
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (request, response) => {
+    response.sendFile(__dirname + "/public/index.html");
+});
+
+io.on("connection", (client) => {
+    console.log("Connected...");
+    client.emit("acknowledge", { message: "Connection established!" });
+
+    client.on("MsgToServer", (chatterName, message) => {
+        console.log(chatterName + " says : " + message);
+        client.emit("MsgToClient", 'Me', message);
+        client.broadcast.emit("MsgToClient", chatterName, message);
+    });
+});
+
+server.listen(port, () => {
+    console.log("Socket server started at port : " + port);
+});
